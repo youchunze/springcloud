3ddf979d4bc08200990a04d9c24810a0501ad74d
paul <396761221@qq.com>
2019-06-29 11:22:51 +0800

    https://blog.csdn.net/forezp/article/details/70148833

---
+++ springcloud/springcloud/eureka-server/src/main/java/cn/ycz/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package cn.ycz.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
