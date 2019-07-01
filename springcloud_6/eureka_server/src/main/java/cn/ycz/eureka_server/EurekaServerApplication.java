e51976255d727ed55fa2d4d943a1666fcc142f3f
paul <396761221@qq.com>
2019-07-01 16:56:18 +0800

    7

--- springcloud_6/eureka_server/src/main/java/cn/ycz/eureka_server/EurekaServerApplication.java
+++ springcloud_6/eureka_server/src/main/java/cn/ycz/eureka_server/EurekaServerApplication.java
@@ -1,17 +1,15 @@
-package cn.ycz.config_server;
+package cn.ycz.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-public class ConfigServerApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
