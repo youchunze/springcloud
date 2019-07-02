7de5cb2d2a49217ec1394b7fadbda30e177fb965
paul <958900021@qq.com>
2019-07-02 17:31:20 +0800

    "7"

--- springcloud_6/eureka_server/src/main/java/cn/ycz/eureka_server/EurekaServerApplication.java
+++ springcloud_6/eureka_server/src/main/java/cn/ycz/eureka_server/EurekaServerApplication.java
@@ -2,10 +2,11 @@ package cn.ycz.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
